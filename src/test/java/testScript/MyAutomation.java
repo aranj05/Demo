42e720735418111a0cdb0306622bebdaaf760fd4
aranj05 <emailid>
2024-04-18 07:12:22 +0000

    Test with older browser version

--- src/test/java/testScript/MyAutomation.java
+++ src/test/java/testScript/MyAutomation.java
@@ -2,12 +2,18 @@ package testScript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MyAutomation {
 
 	public static void main(String[] args) {
 		
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions options= new ChromeOptions();
+		options.setBrowserVersion("115");       //run test on chrome version-115
+		
+		WebDriver driver=new ChromeDriver(options);
+		
+		// WebDriver driver=new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("Page Title.."+ driver.getTitle());
