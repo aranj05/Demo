d77734cd4b554f2aeb1fb9d2edfb743c3c7d7a87
aranj05 <emailid>
2024-04-18 07:31:03 +0000

    New changes

--- src/test/java/testScript/MyAutomation.java
+++ src/test/java/testScript/MyAutomation.java
@@ -1,6 +1,9 @@
 package testScript;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -8,16 +11,21 @@ public class MyAutomation {
 
 	public static void main(String[] args) {
 		
-		ChromeOptions options= new ChromeOptions();
-		options.setBrowserVersion("115");       //run test on chrome version-115
+		//ChromeOptions options= new ChromeOptions();
+		//options.setBrowserVersion("115");       //run test on chrome version-115
 		
-		WebDriver driver=new ChromeDriver(options);
+		//WebDriver driver=new ChromeDriver(options);
 		
-		// WebDriver driver=new EdgeDriver();
+	   WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("Page Title.."+ driver.getTitle());
-
+		
+		WebElement src_box= driver.findElement(By.id("APjFqb"));
+		src_box.sendKeys("Java Tutorial");
+		src_box.sendKeys(Keys.ENTER);
+		
+		System.out.println("Page Title.."+ driver.getTitle());
 	}
 
 }
